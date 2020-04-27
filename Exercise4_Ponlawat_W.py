1d9d29f3d91f444ae62f75efd09300bd87083847
tonpatter00 <63650290+tonpatter00@users.noreply.github.com>
2020-04-28 00:18:59 +0700

    Delete Exercise4_Ponlawat_W.py

--- Exercise4_Ponlawat_W.py
+++
@@ -1,9 +0,0 @@
-print("---Your Score---")
-a = 80
-b = 75
-c = 85
-d = 90
-print("Foundation English               :",a,"คะแนน")
-print("General Business                 :",b,"คะแนน")
-print("Introduction to Computer Systems :",c,"คะแนน")
-print("Computer Programming             :",d,"คะแนน")
